cb0b82e9e423294672fa20c16776f1c4d0e4e4ed
Hammad Babar <33322170+hammadbabar2000@users.noreply.github.com>
2022-03-16 12:51:44 -0400

    Add files via upload

---
+++ Start Page Project/homepage-master/bookmarks.js
@@ -0,0 +1,27 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Gmail", url: "https://inbox.google.com" },
+      { name: "Outlook", url: "https://outlook.live.com/mail/0/inbox" },
+      { name: "GitHub", url: "https://github.com" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "Netflix", url: "https://netflix.com" },
+      { name: "Amazon Prime", url: "https://www.amazon.com/Amazon-Video" },
+    ],
+  },
+  {
+    title: "Reddit",
+    links: [
+      { name: "/r/me_irl", url: "https://reddit.com/r/me_irl" },
+      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
+      { name: "/r/news", url: "https://reddit.com/r/news" },
+    ],
+  },
+];
