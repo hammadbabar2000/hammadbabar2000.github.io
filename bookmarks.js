132773338f94b1da9583c5cbbd92f845aaf06663
Hammadbabar2000 <hammadbabar2000@outlook.com>
2023-02-25 23:57:12 -0500

    updated bookmarks and size of bookmark container

--- bookmarks.js
+++ bookmarks.js
@@ -19,6 +19,8 @@ const bookmarks = [
       { name: "Twitch", url: "https://twitch.tv" },
       { name: "Disney+", url: "https://disneyplus.com/" },
       { name: "FloatPlane", url: "https://floatplane.com/"},
+      { name: "Cruncyroll", url: "https://cruncyroll.com/"},
+      { name: "AnimeShow", url: "https://animeshow.tv/"}
     ],
   },
   {
